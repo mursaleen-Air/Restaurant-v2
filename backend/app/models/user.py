95587a6e16d83cbecee0c728f0f3a4d2df82d524
mursaleen-Air <mursaleen231213@example.com>
2026-02-05 23:02:14 +0500

    ap.js

--- backend/app/models/user.py
+++ backend/app/models/user.py
@@ -1,11 +1,13 @@
 from sqlalchemy import Column, Integer, String, DateTime
 from sqlalchemy.sql import func
-from app.db.base_class import Base
+from app.db.base import Base
 
 class User(Base):
+    __tablename__ = "user"
+
     id = Column(Integer, primary_key=True, index=True)
-    name = Column(String, index=True)
+    name = Column(String)
     email = Column(String, unique=True, index=True, nullable=False)
     hashed_password = Column(String, nullable=False)
-    role = Column(String, default="customer") # 'admin' or 'customer'
+    role = Column(String, default="customer")
     created_at = Column(DateTime(timezone=True), server_default=func.now())
